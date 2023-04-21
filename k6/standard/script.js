737446688d2a96424c961291d271dcba4134ea0d
Mike Nielsen <mmni@protonmail.com>
2023-04-21 11:43:07 +0200

    upgraded gradle

--- k6/standard/script.js
+++ k6/standard/script.js
@@ -4,8 +4,7 @@ export const options = {
     // insecureSkipTLSVerify: true
 };
 export default function() {
-    // let res = http.get("http://localhost:8080/api/navigation");
-    let res = http.get("https://34.160.93.194:443/connect?login=9000000001234&password=ba367bc0ea8b14bf701f9b9272c2cbde&data=<postman>somedata</postman>");
+    let res = http.get("http://localhost:8080/api/navigation");
 
     check(res, {
         "is status 200": (r) => r.status === 200
